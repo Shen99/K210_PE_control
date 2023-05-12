bdcbee7f28add812b46e3308a0e7e1cfba3a3705
Vertexi <778950965@qq.com>
2023-05-12 18:32:54 +0800

    fix bug in build.py

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import shutil, errno, os
 
 # you need change this to your real port
-com = "COM8"
+com = "COM7"
 
 def copyanything(src, dst):
     try:
@@ -17,6 +17,6 @@ if (os.path.exists(source_des)):
     shutil.rmtree("./kendryte-standalone-sdk/src/test")
 copyanything("./src/test", "./kendryte-standalone-sdk/src/test/")
 
-os.system('cmake --no-warn-unused-cli -DPROJ=test -DCMAKE_BUILD_TYPE:STRING=Debug -S ./kendryte-standalone-sdk -B ./build -G "MinGW Makefiles"')
+os.system('cmake ./kendryte-standalone-sdk -B build -DPROJ=test -G "MinGW Makefiles"')
 os.system('cmake --build build')
-os.system('kflash -p ' + com + ' -b 2000000 -f 0 --verbose --noansi --terminal ./build/test.bin')
+os.system('kflash -p ' + com + ' -b 3000000 --verbose --noansi --terminal build/test.bin')
